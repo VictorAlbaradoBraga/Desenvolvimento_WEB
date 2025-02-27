624e07eb3b3dfeb6ded6eca848001f3f436d9888
Victor Albarado <99058838+VictorAlbaradoBraga@users.noreply.github.com>
2025-02-27 18:35:29 -0400

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
 
         try {
             // Enviando dados para o backend (requisição POST para login)
-            const response = await fetch('https://back-prodfunc-omega.vercel/api/auth', {
+            const response = await fetch('https://back-prodfunc-omega.vercel.app/api/auth', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
